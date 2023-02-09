5d40ed9c23eb4328273c5307e4fcda34f95c331d
Zync <Lukasjuulnielsen@gmail.com>
2023-02-09 19:48:22 +0100

    Map Generation begun.

---
+++ HoboKnight/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+
+    public GameObject[] FourXThree;
+    public GameObject[] SixXThree;
+    public GameObject[] EightXThree;
+    public GameObject[] TenXThree;
+
+    int[,] HouseSizes = new int[,] { { 4, 3 }, { 6, 3 }, { 8, 3 }, { 10, 3 } };
+
+    void Start()
+    { 
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
